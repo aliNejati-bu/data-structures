55c46fae10810b02a09b0f93bf541e7f6cc9fcb4
alinejati-bu <electrocellco@gmail.com>
2022-01-06 23:40:47 +0330

    add infixToPrefix

--- src/com/ali/stacks/applications/StackApplications.java
+++ src/com/ali/stacks/applications/StackApplications.java
@@ -11,12 +11,13 @@ public class StackApplications {
         return switch (operator) {
             case '+', '-' -> 1;
             case '*', '/' -> 2;
+            case '^' -> 3;
             default -> 0;
         };
     }
 
     static boolean pushable(Character inStack, Character newOperator) {
-        if (inStack == null){
+        if (inStack == null) {
             return true;
         }
         if (newOperator == '^' || newOperator == '(')
@@ -24,6 +25,19 @@ public class StackApplications {
         int newValue = getOperatorValue(newOperator);
         int inStackValue = getOperatorValue(inStack);
         return newValue > inStackValue;
+    }
+
+
+    static boolean infixToPrefixPushable(Character inStack, Character newOperator) {
+        if (newOperator == ')' || inStack == null) {
+            return true;
+        }
+        if (newOperator == '^' && inStack == '^') {
+            return false;
+        }
+        int newValue = getOperatorValue(newOperator);
+        int inStackValue = getOperatorValue(inStack);
+        return newValue >= inStackValue;
     }
 
     public static String infixToPostfix(String infixExpression, IStack<Character> stack) {
@@ -36,29 +50,63 @@ public class StackApplications {
                     result.append(c);
                     c = stack.pop();
                 }
-            }
-            else if (isOperator(currChar)) {
+            } else if (isOperator(currChar)) {
                 Character c = stack.pop();
                 while (!pushable(c, currChar)) {
                     result.append(c);
                     c = stack.pop();
                 }
-                if (c != null){
+                if (c != null) {
                     stack.push(c);
                 }
                 stack.push(currChar);
-            }
-            else {
+            } else {
                 result.append(currChar);
             }
         }
-        if (!stack.isEmpty()){
+        if (!stack.isEmpty()) {
             Character c = stack.pop();
-            while (c!=null){
+            while (c != null) {
                 result.append(c);
                 c = stack.pop();
             }
         }
         return result.toString();
     }
+
+
+    public static String infixToPrefix(String infixExpression, IStack<Character> stack) {
+        StringBuilder result = new StringBuilder();
+        for (int i = infixExpression.length() - 1; i >= 0; i--) {
+            char currChar = infixExpression.charAt(i);
+            if (currChar == '(') {
+                char c = stack.pop();
+                while (c != ')') {
+                    result.append(c);
+                    c = stack.pop();
+                }
+            } else if (isOperator(currChar)) {
+                Character c = stack.pop();
+                while (!infixToPrefixPushable(c, currChar)) {
+                    result.append(c);
+                    c = stack.pop();
+                }
+                if (c != null) {
+                    stack.push(c);
+                }
+                stack.push(currChar);
+            } else {
+                result.append(currChar);
+            }
+        }
+        if (!stack.isEmpty()) {
+            Character c = stack.pop();
+            while (c != null) {
+                result.append(c);
+                c = stack.pop();
+            }
+        }
+        return result.reverse().toString();
+    }
+
 }
