2ef76bf9fa02c85573a346351b9a5e6a436dbf9e
alinejati-bu <electrocellco@gmail.com>
2022-01-06 13:39:05 +0330

    add node

---
+++ src/com/ali/utils/Node.java
@@ -0,0 +1,14 @@
+package com.ali.utils;
+
+public class Node<E> {
+    public Node<E> next = null;
+    public Node<E> prev = null;
+    public E data;
+
+
+    public Node(Node<E> next, Node<E> prev, E data) {
+        this.next = next;
+        this.prev = prev;
+        this.data = data;
+    }
+}
